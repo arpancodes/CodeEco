71a9b8524ee6856da422eb71bc51b0810faa7a3d
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-08-03 06:57:02 +0530

    add India districts map

--- script.js
+++ script.js
@@ -56,76 +56,82 @@ $.ajax({
 //  "Puducherry"
 // ]
 
-var states = ["Karnataka", "Tamil Nadu"]
+var states = ["Karnataka", "Tamil Nadu"];
 
- autocomplete(document.getElementById("myInput"), states);
+autocomplete(document.getElementById("myInput"), states);
 
- function autocomplete(inp, arr) {
+function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              const url = b.textContent.split(' ').join('').toLowerCase();
-              window.location = `./${url}.html`
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
+  inp.addEventListener("input", function (e) {
+    var a,
+      b,
+      i,
+      val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          const url = b.textContent.split(" ").join("").toLowerCase();
+          window.location = `./${url}.html`;
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
               (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
         increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
         decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
       }
+    }
   });
   function addActive(x) {
     /*a function to classify an item as "active":*/
@@ -133,7 +139,7 @@ var states = ["Karnataka", "Tamil Nadu"]
     /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
+    if (currentFocus < 0) currentFocus = x.length - 1;
     /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
@@ -149,12 +155,39 @@ var states = ["Karnataka", "Tamil Nadu"]
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
+        x[i].parentNode.removeChild(x[i]);
+      }
     }
   }
-}
-/*execute a function when someone clicks in the document:*/
-document.addEventListener("click", function (e) {
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
     closeAllLists(e.target);
-});
-} 
+  });
+}
+
+const btn2017 = document.querySelector("#btn-2017");
+const btn2013 = document.querySelector("#btn-2013");
+
+function selectYear17() {
+  btn2017.classList.add("selected");
+  btn2013.classList.remove("selected");
+  document.querySelector("#map-dist").classList.add("map-hide");
+  document.querySelector("#map").classList.remove("map-hide");
+  document.querySelector("#map").classList.add("map-show");
+  document.querySelector("#totalStages13").style.display = "none";
+  document.querySelector("#totalStages").style.display = "flex";
+}
+
+function selectYear13() {
+  btn2017.classList.remove("selected");
+  btn2013.classList.add("selected");
+  document.querySelector("#map").classList.add("map-hide");
+  document.querySelector("#map-dist").classList.remove("map-hide");
+  document.querySelector("#map-dist").classList.add("map-show");
+  document.querySelector("#totalStages13").style.display = "flex";
+  document.querySelector("#totalStages").style.display = "none";
+}
+selectYear17();
+btn2017.addEventListener("click", selectYear17);
+btn2013.addEventListener("click", selectYear13);
+
--- stateMap.js
+++ stateMap.js
@@ -5,13 +5,17 @@
     .defer(d3.csv, "2013data.csv")
     .defer(d3.csv, "postmonsoon.csv")
     .defer(d3.csv, "premonsoon.csv")
+    .defer(d3.csv, "statedata2017.csv")
+    .defer(d3.csv, "districtdata2017.csv")
     .await(function (
       error,
       topoMap,
       data2017,
       data2013,
       postmonsoon,
-      premonsoon
+      premonsoon,
+      statedata2017,
+      districtdata2017
     ) {
       if (error) throw error;
 
@@ -23,7 +27,7 @@
       var keraladata13 = data2013.filter(element => element['Name of State'] === 'KERALA')
       var goadata = data2017.filter(element => element['Name of State'] === 'GOA')
       var goadata13 = data2013.filter(element => element['Name of State'] === 'GOA')
-
+      
       localStorage.setItem("goadata", JSON.stringify(goadata))
       localStorage.setItem("goadata13", JSON.stringify(goadata13))
       localStorage.setItem("keraladata", JSON.stringify(keraladata))
@@ -37,17 +41,28 @@
       localStorage.setItem("postmonsoon", JSON.stringify(postmonsoon));
       localStorage.setItem("premonsoon", JSON.stringify(premonsoon));
 
-      data2017And2013Creator(data2017, data2013);
+      data2017And2013Creator(data2017, data2013, statedata2017, districtdata2017);
 
       var states = topojson.feature(
         topoMap,
-        topoMap.objects.ne_10m_admin_1_India_Official
+        topoMap.objects.states
         // topoMap.objects.india
       );
 
+      var districts = topojson.feature(
+        topoMap,
+        topoMap.objects.districts
+        // topoMap.objects.india
+      );
+
+      console.log(states)
+      console.log(districts)
+
       // Map render
       var map = stateMap(states.features).width(800).height(700).scale(1200);
+      var mapdist = districtMap(districts.features).width(800).height(700).scale(1200);
       d3.select("#map").call(map);
+      d3.select("#map-dist").call(mapdist);
     });
 })();
 
@@ -64,6 +79,7 @@ function getSum(total, num) {
 
 let selected2017statedata = {};
 let allstate2017data = {};
+let alldist2017data = {};
 let allstate2013data = {};
 let groundWater2017 = 0;
 let groundWater2013 = 0;
@@ -71,35 +87,41 @@ let extractWater2017 = 0;
 let extractWater2013 = 0;
 let extractPercent2017 = 0;
 let extractPercent2013 = 0;
-function data2017And2013Creator(data2017, data2013) {
+function data2017And2013Creator(data2017, data2013, statedata2017, districtdata2017) {
   allstate2017data = groupBy(data2017, "Name of State");
+  alldist2017data = groupBy(data2017, "Name of District");
   allstate2013data = groupBy(data2013, "Name of State");
+  onlystate2017data = groupBy(statedata2017, "State");
+  onlydistrict2017data = groupBy(districtdata2017, "District");
 }
 
+
 function select2017statedata(statename) {
-  groundWater2017 = allstate2017data[statename].sum(
+  console.log(allstate2017data)
+  console.log(statename)
+  groundWater2017 = allstate2017data[statename.toUpperCase()].sum(
     "Total Annual Ground Water Recharge(Ham)"
   );
-  extractWater2017 = allstate2017data[statename].sum(
+  extractWater2017 = allstate2017data[statename.toUpperCase()].sum(
     "Total Current Annual Ground Water Extraction(Ham)"
   );
   extractPercent2017 =
-    allstate2017data[statename].sum("Stage of Ground Water Distribution (%)") /
-    allstate2017data[statename].length;
+    allstate2017data[statename.toUpperCase()].sum("Stage of Ground Water Development (%)") /
+    allstate2017data[statename.toUpperCase()].length;
 
-  groundWater2013 = allstate2013data[statename].sum(
+  groundWater2013 = allstate2013data[statename.toUpperCase()].sum(
     "Total Annual Ground Water Recharge(Ham)"
   );
-  extractWater2013 = allstate2013data[statename].sum(
+  extractWater2013 = allstate2013data[statename.toUpperCase()].sum(
     "Total Current Annual Ground Water Extraction(Ham)"
   );
   extractPercent2013 =
-    allstate2013data[statename].sum("Stage of Ground Water Distribution (%)") /
-    allstate2013data[statename].length;
+    allstate2013data[statename.toUpperCase()].sum("Stage of Ground Water Development (%)") /
+    allstate2013data[statename.toUpperCase()].length;
 
-  groundWaterChart(groundWater2013, groundWater2017, statename);
-  extractWaterChart(extractWater2013, extractWater2017, statename);
-  extractPercentChart(extractPercent2013, extractPercent2017, statename);
+  groundWaterChart(groundWater2013, groundWater2017, statename.toUpperCase());
+  extractWaterChart(extractWater2013, extractWater2017, statename.toUpperCase());
+  extractPercentChart(extractPercent2013, extractPercent2017, statename.toUpperCase());
 }
 
 function groundWaterChart(groundWater2013, groundWater2017, statename) {
@@ -209,7 +231,7 @@ function extractPercentChart(
       type: "pie",
     },
     title: {
-      text: statename + " Stage of Ground Water Distribution (%)",
+      text: statename + " Stage of Ground Water Development (%)",
     },
     exporting: {
       enabled: false,
@@ -297,30 +319,37 @@ function stateMap(states) {
       selectState
         .append("path")
         .style("fill", function (d) {
-          return color[Math.floor(Math.random() * 5)];
+          renderStatesStats()
+          return paintState(d.properties.st_nm.toUpperCase())
+        }).style("stroke", function(d){
+          return "#ffffff"
+        }).style("stroke-width", function(d){
+          return "1.5px"
         })
         .attr("d", path)
         .on("mousedown", function (d) {
-          const url = d.properties.name.toLowerCase().split(' ').join('-');
+          const url = d.properties.st_nm.toLowerCase().split(' ').join('');
+          // alert(url)
           window.location = `/${url}.html`
-          //alert(d.properties.name);
+          //alert(d..properties.st_nm);
           console.log(d);
         })
         .on("mouseover", function (d) {
           //   document.querySelector(".stats").innerHTML = `
           //   <ul>
-          //   <li>Name: ${d.properties.name}</li>
+          //   <li>Name: ${d.properties.st_nm}</li>
           //   <li>Population: ${d.properties.population}</li>
           //   <li>Area: ${d.properties.area}</li>
           // </ul>
           //   `;
           console.log(d3.event.pageY);
-          select2017statedata(d.properties.name);
+          console.log(d)
+          select2017statedata(d.properties.st_nm);
           div.transition().duration(200).style("opacity", 0.9);
-          div
-            .html(d.properties.name + "<br/>Area: " + d.properties.area)
-            .style("left", d3.event.pageX + "px")
-            .style("top", d3.event.pageY - 28 + "px");
+          // div
+          //   .html(d.properties.st_nm)
+          //   .style("left", d3.event.pageX + "px")
+          //   .style("top", d3.event.pageY - 28 + "px");
         })
         .on("mouseout", function (d) {
           div.transition().duration(500).style("opacity", 0);
@@ -338,9 +367,9 @@ function stateMap(states) {
           return "translate(" + path.centroid(d) + ")";
         })
         .attr("dy", ".35em")
-        .text(function (d) {
-          return d.properties.name;
-        });
+        // .text(function (d) {
+        //   return d.properties.st_nm;
+        // });
     });
   } // render
   render.height = function (value) {
@@ -361,3 +390,210 @@ function stateMap(states) {
 
   return render;
 }
+
+function districtMap(states) {
+  var width = 800,
+    height = 700,
+    scale = 1200;
+  var color = ["#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3"];
+
+  // Define the div for the tooltip
+  var div = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  function render(selection) {
+    selection.each(function () {
+      d3.select(this).select("svg").remove();
+      var svg = d3
+        .select(this)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+      var projection = d3.geo
+        .mercator()
+        .center([83, 23])
+        .scale(scale)
+        .translate([width / 2, height / 2]);
+
+      var path = d3.geo.path().projection(projection);
+      var selectState = svg
+        .selectAll("g")
+        .data(states)
+        .enter()
+        .append("g")
+        .attr("class", "state");
+
+      selectState
+        .append("path")
+        .style("fill", function (d) {
+          // distColour(d.properties.district);
+          renderDistStats()
+          return paintDistrict(d.properties.district)
+        }).style("stroke", function(d){
+          return "rgba(255,255,255,.5)"
+        }).style("stroke-width", function(d){
+          return "1px"
+        })
+        .attr("d", path)
+        .on("mousedown", function (d) {
+          const url = d.properties.st_nm.toLowerCase().split(' ').join('');
+          // alert(url)
+          window.location = `/${url}.html`
+          //alert(d..properties.st_nm);
+          console.log(d);
+        })
+        .on("mouseover", function (d) {
+          //   document.querySelector(".stats").innerHTML = `
+          //   <ul>
+          //   <li>Name: ${d.properties.st_nm}</li>
+          //   <li>Population: ${d.properties.population}</li>
+          //   <li>Area: ${d.properties.area}</li>
+          // </ul>
+          //   `;
+          console.log(d3.event.pageY);
+          console.log(d)
+          select2017statedata(d.properties.st_nm);
+          div.transition().duration(200).style("opacity", 0.9);
+          // div
+          //   .html(d.properties.st_nm)
+          //   .style("left", d3.event.pageX + "px")
+          //   .style("top", d3.event.pageY - 28 + "px");
+        })
+        .on("mouseout", function (d) {
+          div.transition().duration(500).style("opacity", 0);
+        });
+
+      svg
+        .selectAll("text")
+        .data(states)
+        .enter()
+        .append("text")
+        .attr("class", function (d) {
+          return "label " + d.id;
+        })
+        .attr("transform", function (d) {
+          return "translate(" + path.centroid(d) + ")";
+        })
+        .attr("dy", ".35em")
+        // .text(function (d) {
+        //   return d.properties.st_nm;
+        // });
+    });
+  } // render
+  render.height = function (value) {
+    if (!arguments.length) return height;
+    height = value;
+    return render;
+  };
+  render.width = function (value) {
+    if (!arguments.length) return width;
+    width = value;
+    return render;
+  };
+  render.scale = function (value) {
+    if (!arguments.length) return scale;
+    scale = value;
+    return render;
+  };
+  return render;
+}
+
+let overexploitedStates = 0;
+let criticalStates = 0;
+let semicriticalStates = 0;
+let safeStates = 0;
+
+function paintState(name){
+  const area = onlystate2017data[name] 
+  let percent;
+  if(area)  {
+    percent = area[0]["Stage of Ground Water Development (%)"]
+  if(percent > 100) {
+    overexploitedStates++;
+    return "rgb(234, 51, 35)"
+  }
+  if(percent > 90) {
+    criticalStates++
+    return "rgb(140, 28, 100)"
+  }
+  if(percent > 70) {
+    semicriticalStates++
+    return "rgb(93, 21, 147)"
+  }
+  if(percent > 30) {
+    safeStates++
+    return "rgb(44, 20, 196)"
+  }
+  if(percent > 20) {
+    safeStates++
+    return "rgb(0, 24, 245)"
+  }
+  safeStates++
+  return "rgb(44, 20, 196)"
+}
+  safeStates++
+  return "blue"
+}
+
+let overexploitedDist = 0;
+let criticalDist = 0;
+let semicriticalDist = 0;
+let safeDist = 0;
+
+function paintDistrict(name){
+
+  const area = onlydistrict2017data[name]
+  let percent;
+  if(area)  {
+    percent = area[0]["Stage of Ground Water Development (%)"]
+    if(percent > 100) {
+      overexploitedDist++;
+      return "rgb(234, 51, 35)"
+    }
+    if(percent > 90) {
+      criticalDist++
+      console.log(criticalDist)
+      return "rgb(140, 28, 100)"
+    }
+    if(percent > 70) {
+      semicriticalDist++
+      return "rgb(93, 21, 147)"
+    }
+    if(percent > 30) {
+      safeDist++
+      return "rgb(44, 20, 196)"
+    }
+    if(percent > 20) {
+      safeDist++
+      return "rgb(0, 24, 245)"
+    }
+    safeDist++
+    return "rgb(44, 20, 196)"
+  }
+    safeDist++
+    return "blue"
+}
+
+function renderDistStats(){
+  document.getElementById("totalStages13").innerHTML = `
+          <div class="stage over-exploited"><h2>Over Exploited</h2><p>${overexploitedDist}</p></div>
+          <div class="stage critical"><h2>Critical</h2><p>${criticalDist}</p></div>
+          <div class="stage semi-critical"><h2>Semi Ciritcal</h2><p>${semicriticalDist}</p></div>
+          <div class="stage safe"><h2>Safe</h2><p>${safeDist}</p></div>
+          `;
+}
+
+function renderStatesStats(){
+  document.getElementById("totalStages").innerHTML = `
+          <div class="stage over-exploited"><h2>Over Exploited</h2><p>${overexploitedStates}</p></div>
+          <div class="stage critical"><h2>Critical</h2><p>${criticalStates}</p></div>
+          <div class="stage semi-critical"><h2>Semi Ciritcal</h2><p>${semicriticalStates}</p></div>
+          <div class="stage safe"><h2>Safe</h2><p>${safeStates}</p></div>
+          `;
+}
+
+renderDistStats()
